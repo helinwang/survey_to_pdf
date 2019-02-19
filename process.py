f93287e54a05bf92d39d21974cc0eb7ecf2865b0
Helin Wang <ustc.harry@gmail.com>
2019-02-18 20:03:01 -0800

    works

--- process.py
+++ process.py
@@ -9,7 +9,9 @@ prop = mfm.FontProperties(fname="kai.ttf")
 import matplotlib.pyplot as plt
 
 email_field = 'Email to be evaluated (only enter your own Email if this is a self-evaluation)'
-skip_fields = {'Timestamp', 'This quiz is intended for', 'Name to be evaluated (only enter your own Email if this is a self-evaluation)', email_field}
+name_field = 'Name to be evaluated (only enter your own Email if this is a self-evaluation)'
+intend_for_field = 'This quiz is intended for'
+skip_fields = {'Timestamp', intend_for_field, email_field, name_field}
 
 pdf = FPDF('P', 'mm', 'Letter')
 pdf.add_font('kai', '', 'kai.ttf',True)
@@ -20,7 +22,7 @@ IMAGE_HEIGHT = 100
 HEADER_HEIGHT = 15
 COLUMNS = 2
 
-def plot(x, y, question, answers):
+def plot(x, y, question, answers, name):
     d = {}
     for ans in answers:
         if ans not in d:
@@ -37,30 +39,47 @@ def plot(x, y, question, answers):
     plt.text(-0.8, 1.2, question, fontproperties=prop)
     plt.axis('equal')
     plt.setp(texts, fontproperties=prop)
-    file_name = question+'.png'
+    file_name = name+str(x)+'.'+str(y)+'.png'
     plt.savefig(file_name)
     plt.clf()
     pdf.image(file_name,x,y,IMAGE_WIDTH,IMAGE_HEIGHT)
 
 
 def persons(rows):
-    pass
-
-def print_person(questions, rows):
-    results = {}
+    ps = {}
     for row in rows:
+        email = row[email_field].lower()
+        if email not in ps:
+            ps[email] = []
+        ps[email].append(row)
+    ret = []
+
+    for _, v in ps.items():
+        ret.append(v)
+    return ret
+
+def print_person(questions, name, rows):
+    results = {}
+    feedbacks_from_other = 0
+    for row in rows:
+        if row[intend_for_field].lower() == "myself":
+            continue
+
+        feedbacks_from_other += 1
         for question in questions:
             if question not in results:
                 results[question] = []
             results[question].append(row[question])
 
     pdf.add_page()
-    pdf.cell(0, 0, txt=u'测试')
+    pdf.cell(0, 0, txt=name+', '+str(feedbacks_from_other) + " feedbacks from others")
     i = 0
+
     for question in questions:
+        print(name, results[question])
         x = (i % COLUMNS) * IMAGE_WIDTH
         y = int(i / COLUMNS) * IMAGE_HEIGHT + HEADER_HEIGHT
-        plot(x, y, question, results[question])
+        plot(x, y, question, results[question], name)
         i+=1
 
 
@@ -75,6 +94,9 @@ with open('quiz.csv') as csvfile:
     rows = []
     for row in reader:
         rows.append(row)
-        
-    print_person(questions, rows)
+
+    ps = persons(rows)
+    for prs in ps:
+        print_person(questions, prs[0][name_field], prs)
+
     pdf.output("pages.pdf", "F")
